c287ce40cd218f98098f253ce91919dfe49c8fd7
sakshishetty1231997@gmail.com <SakshiShetty641>
2024-07-15 11:41:34 +0530

    Variables in Go

---
+++ Basics/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//1st way
+	var num int = 30
+	fmt.Println("Number is ", num)
+
+	//2nd way
+	var name = "Tom"
+	fmt.Println("Name is ", name)
+
+	//3rd way
+	age := 28
+	fmt.Println("Age  is ", age)
+
+}
